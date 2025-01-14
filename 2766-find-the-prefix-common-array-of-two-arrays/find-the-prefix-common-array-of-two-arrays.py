4064a444b82ca0bf591b63a0b5fadf2887893248
Dev Shrut Jain <138303636+jaindevshrut@users.noreply.github.com>
2025-01-14 23:13:52 +0530

    Time: 6 ms (77.60%) | Memory: 18 MB (9.90%) - LeetSync

--- 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.py
+++ 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.py
@@ -1,21 +1,15 @@
 class Solution:
     def findThePrefixCommonArray(self, A: List[int], B: List[int]) -> List[int]:
+        # frequency wala badiya h usme A,B dono permutations h toh hame track rahege numbers ke if kisi ki freq 2 ho gyi mtlb vo dno me aa gaya h
         ans = []
-        x,y = set(),set()
-        if A[0] == B[0]:
-            ans.append(1)
-        else:
-            ans.append(0)
-            x.add(A[0])
-            y.add(B[0])
-        for i in range(1,len(A)):
-            if A[i] == B[i]:
-                ans.append(ans[-1]+1)
-            else:
-                x.add(A[i])
-                y.add(B[i])
-                t = x.intersection(y)
-                x -= t
-                y -= t
-                ans.append(ans[-1]+len(t))
+        freq = [0]*(len(A)+1)
+        curr = 0
+        for i in range(len(A)):
+            freq[A[i]] += 1
+            if freq[A[i]] == 2:
+                curr += 1
+            freq[B[i]] += 1
+            if freq[B[i]] == 2:
+                curr += 1
+            ans.append(curr)
         return ans
